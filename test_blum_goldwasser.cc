6f157b7e39a9e0a4e89fd893056aed268efa2424
Jordan Charest <jordancharest3@gmail.com>
2018-11-04 17:54:30 -0500

    Assert that original plaintext matches the decrypted result - passes test case

--- test_blum_goldwasser.cc
+++ test_blum_goldwasser.cc
@@ -1,15 +1,12 @@
 #include <bitset>
 #include <cstdlib>
 #include <iostream>
-#include <string>
 #include <tuple>
-
 
 #include "blum_goldwasser.h"
 
-
 int main(int argc, char** argv) {
-  std::bitset<20> plaintext(std::string("10011100000100001100"));
+  std::bitset<20> plaintext("10011100000100001100");
   int p = 499;
   int q = 547;
   BG::Cipher cipher(p, q);
@@ -20,15 +17,14 @@ int main(int argc, char** argv) {
   unsigned long long XL; 
   std::tie(ciphertext, XL) = cipher.encrypt(plaintext, X0);
   std::cout << "Ciphertext: " << ciphertext << "\n";
-  std::cout << "XL: " << XL << "\n";
-
 
   // decrypt
-  cipher.decrypt(ciphertext, XL);
+  std::bitset<20> decrypted = cipher.decrypt(ciphertext, XL);
+
+  // original plaintext and decrypted text should match
+  std::cout << "Plaintext: " << plaintext << "\n";
+  std::cout << "Decrypted: " << decrypted << "\n";
+  assert(plaintext == decrypted);
     
-
-
-
-
   return EXIT_SUCCESS;
 }
