e22c2de177fbe9e2a99574f05647d4d54a0c7b31
Jordan Charest <jordancharest3@gmail.com>
2018-11-04 17:44:20 -0500

    Added decryption and modular inverse helper

--- blum_goldwasser.h
+++ blum_goldwasser.h
@@ -1,15 +1,21 @@
 #include <bitset>
 #include <boost/multiprecision/cpp_int.hpp>
-#include <boost/multiprecision/integer.hpp>
-#include <boost/math/special_functions/pow.hpp>
-
-// #include <boost/math/special_functions/mpow.hpp>
 #include <cstdlib>
 #include <tuple>
 
-
 namespace mp = boost::multiprecision;
 
+// ----------------------------------------------------------------------------
+int mod_inverse(int a, int mod) {
+  int i = ceil((mod-1)/a);
+  for (; i < mod; ++i) {
+    if ((a * i) % mod == 1)
+      return i;
+  }
+  return 1;
+}
+
+// ============================================================================
 namespace BG {  // Blum-Goldwasser 
 
 class Cipher {
@@ -29,12 +35,12 @@ class Cipher {
                                                     std::bitset<size> plaintext, 
                                                     unsigned long long X);
   template <std::size_t size>
-  void decrypt(std::bitset<size> ciphertext, unsigned long long X);
+  std::bitset<size> decrypt(std::bitset<size> ciphertext, int X);
 
-  // decryption helper
-  mp::cpp_int compute_r(int prime, int len, int y);
 
  private:
+  // decryption helper - arbitrary precision modular arithmetic
+  int compute_r(int prime, int len, int y);
 
   // only use const member variables in classes where moving is forbidden
   const int p_;
@@ -61,68 +67,46 @@ std::tuple<std::bitset<size>, unsigned long long> Cipher::encrypt(
     X = (X * X) % N_;
   }
 
+  std::cout << "Random bits: " << random_bits << "\n";
+
   std::bitset<size> ciphertext = plaintext ^ random_bits;
   return std::make_tuple(ciphertext, X);
 }
 
 // ----------------------------------------------------------------------------
 template <std::size_t size>
-void Cipher::decrypt(std::bitset<size> ciphertext, unsigned long long X) {
-  mp::cpp_int rp = this->compute_r(p_, ciphertext.size(), X);
-  mp::cpp_int rq = this->compute_r(q_, ciphertext.size(), X);
+std::bitset<size> Cipher::decrypt(std::bitset<size> ciphertext, int XL) {
 
+  // calculate the X0 that the encryptor used
+  int rp = this->compute_r(p_, ciphertext.size(), XL);
+  int rq = this->compute_r(q_, ciphertext.size(), XL);
   std::cout << "rp: " << rp << "\n";
   std::cout << "rq: " << rq << "\n";
+  unsigned long long X;
+  X = ((q_ * mod_inverse(q_, p_)) * rp + p_ * mod_inverse(p_, q_) * rq) % N_;
+  std::cout << "X: " << X << "\n";
 
+
+  // Use X0 to initiate Blum Blum Shub random bit generation
+  std::bitset<size> random_bits;
+  for (int i = 0; i < size; ++i) {
+    random_bits[i] = X & 1;
+    X = (X * X) % N_;
+  }
+
+  std::cout << "Random bits: " << random_bits << "\n";
+
+  return ciphertext ^ random_bits;
 }
 
 // ----------------------------------------------------------------------------
-mp::cpp_int Cipher::compute_r(int prime, int len, int y) {
-
+int Cipher::compute_r(int prime, int len, int y) {
   mp::cpp_int temp = ((prime + 1)/4);
   mp::cpp_int exp = mp::pow(temp, len);
 
   mp::cpp_int r = mp::powm(mp::cpp_int(y), exp, prime);
-  return r;
+  return static_cast<int>(r);
 }
-
-
-// // right to left binary method of modular exponentiation
-// mp::cpp_int modular_exponentiation(mp::cpp_int base, mp::cpp_int exp, mp::cpp_int mod) {
-//   if (mod == 1) return 0;
-//   mp::cpp_int result = 1;
-//   base = base % mod;
-//   while (exp > 0) {
-//     if (exp & 1 == 1)
-//       result = (result * base) % mod;
-
-//     exp >>= 1;
-//     base = (base * base) % mod;
-//   }
-
-//   return result;
-// }
-
-
-
-
-
-// function modular_pow(base, exponent, modulus)
-//     if modulus = 1 then return 0
-//     Assert :: (modulus - 1) * (modulus - 1) does not overflow base
-//     result := 1
-//     base := base mod modulus
-//     while exponent > 0
-//         if (exponent mod 2 == 1):
-//            result := (result * base) mod modulus
-//         exponent := exponent >> 1
-//         base := (base * base) mod modulus
-//     return result
-
-
-
-
-
 
 
 
